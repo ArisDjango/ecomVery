5e891675aac0e50ff2455bd6354a4961efce9dcc
winandiaris <winandiaris@gmail.com>
2021-07-11 17:34:31 +0700

    coverage pipenv fail

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ecomVery
 tes git push
+
+- Instalasi
+    - mkdir ecomvery di local --> Buat repo baru git --> clone repo tsb ke local --> test push
+    - cd ecomvery/ecomvery
+    - pipenv --> pip install pipenv --> touch Pipfile --> pipenv lock --> pipenv install system
+    - django-admin startproject ecomvery .
+    - django-admin startapp store
+- Model
+    - Edit store/models
+    - makemigrations --> migrate
+    - set settings.static/media --> set ecomvery.url
+
+- Admin
+    - python manage.py createsuperuser
+    - store.admin --> register model yg ingin ditampilkan
+- Static/media set
+    - set path di settings.py
+    - setting url media
+- Testing
+
+
---
+++ store/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext as _
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(_("nama-kategori"), max_length=255, db_index=True)
+    slug = models.SlugField(_("slug"), max_length=255, unique=True)
+    
+
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categorys")
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse("Category_detail", kwargs={"pk": self.pk})
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, verbose_name=_("category"), on_delete=models.CASCADE)
+    created_by = models.ForeignKey(User,  verbose_name=_("product_creator"), on_delete=models.CASCADE)
+    title = models.CharField(_("judul"), max_length=255)
+    author = models.CharField(_("pencipta"), max_length=255, default = 'admin')
+    description = models.TextField(_("deskripsi"), blank=True)
+    image = models.ImageField(_("gambar"), upload_to='images/', height_field=None, width_field=None, max_length=None)
+    slug = models.SlugField(_("product-slug"), max_length=255)
+    price = models.DecimalField(_("harga"), max_digits=4, decimal_places=2)
+    in_stock = models.BooleanField(_("stok"), default=True)
+    is_active = models.BooleanField(_("aktif"), default=True)
+    created = models.DateTimeField(_("tanggal_created"), auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(_("tangal_update"), auto_now=True, auto_now_add=False)
+
+
+    class Meta:
+        verbose_name = _("Product")
+        verbose_name_plural = _("Products")
+        ordering = ('-created',)
+
+    def __str__(self):
+        return self.title
+
+    # def get_absolute_url(self):
+    #     return reverse("Product_detail", kwargs={"pk": self.pk})
+
+
+
+
+    
+
+    
+
