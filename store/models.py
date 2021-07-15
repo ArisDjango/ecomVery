093131103a7eb447cbbcb4b60b343900ed75b220
winandiaris <winandiaris@gmail.com>
2021-07-15 07:32:20 +0700

    part 1

--- store/models.py
+++ store/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext as _
+from django.urls import reverse
 
 # Create your models here.
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(is_active=True)
 
 class Category(models.Model):
     name = models.CharField(_("nama-kategori"), max_length=255, db_index=True)
@@ -12,6 +16,10 @@ class Category(models.Model):
     class Meta:
         verbose_name = _("Category")
         verbose_name_plural = _("Categorys")
+    
+    def get_absolute_url(self):
+        return reverse('store:category_list', args=[self.slug])
+
 
     def __str__(self):
         return self.name
@@ -34,11 +42,17 @@ class Product(models.Model):
     created = models.DateTimeField(_("tanggal_created"), auto_now=False, auto_now_add=True)
     updated = models.DateTimeField(_("tangal_update"), auto_now=True, auto_now_add=False)
 
+    objects = models.Manager()
+    products = ProductManager()
+
 
     class Meta:
         verbose_name = _("Product")
         verbose_name_plural = _("Products")
         ordering = ('-created',)
+
+    def get_absolute_url(self):
+        return reverse('store:product_detail', args=[self.slug])
 
     def __str__(self):
         return self.title
--- store/views.py
+++ store/views.py
@@ -9,17 +9,21 @@ def categories(request):
 
 # store/home.html
 def all_products(request):
-    products = Product.objects.all()
-    return render()
+    products = Product.products.all()
+    return render(request, 'store/home.html', {'products': products})
+    # return render(request, 'test.html')
 
-# category_list --> store/products/category.html
-def category_list(request, category_slug=none):
+#  category_list --> store/products/category.html
+def category_list(request, category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(category=category)
-    return render()
+    return render(request, 'store/products/category.html', {'category': category, 'products': products})
 
 # product_detail --> store/products/category.html
 def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'store/products/detail.html', {'product': product})
+
     
 
 
