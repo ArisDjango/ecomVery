bef3d8769e2c7b539401f9a9a281f6fd001baf0c
aris <winandiaris@gmail.com>
2022-01-22 19:19:56 +0700

    refactor-masih banyak bug karena upgrade ke django 4

---
+++ ecommerce/urls.py
@@ -0,0 +1,20 @@
+# import debug_toolbar
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('ecommerce.apps.catalogue.urls', namespace='catalogue')),
+    path('checkout/', include('ecommerce.apps.checkout.urls', namespace='checkout')),
+    path('basket/', include('ecommerce.apps.basket.urls', namespace='basket')),
+    # path('payment/', include('payment.urls', namespace='payment')), # disable during paypal integration part 8
+    path('account/', include('ecommerce.apps.account.urls', namespace='account')),
+    path('orders/', include('ecommerce.apps.orders.urls', namespace='orders')),
+    # path("__debug__/", include(debug_toolbar.urls)),
+    
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/context_processors.py
+++
@@ -1,9 +0,0 @@
-from .models import Category
-
-
-# Pindahan dari view
-def categories(request):
-    return {
-        # 'categories': Category.objects.all()
-        'categories': Category.objects.filter(level=0)
-    }
